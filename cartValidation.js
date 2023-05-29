4d5d19fd24d5ed0b090c30c2ca70c005de440640
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2023-05-29 21:18:14 +0200

    1

--- cartValidation.js
+++ cartValidation.js
@@ -1,13 +1,12 @@
-
 const price = document.querySelector("#price");
 const selectElement = document.querySelector("#select");
 let originalPrice = parseFloat(price.innerHTML);
+
 selectElement.addEventListener("change", function() {
   const selected = selectElement.options[selectElement.selectedIndex].text;
   let updatedPrice = (originalPrice * selected);
   price.innerHTML = updatedPrice;
 })
-
 
 
 const checkoutButton = document.querySelector("#checkoutbutton");
@@ -30,7 +29,6 @@ const errorCity = document.querySelector("#errorCity");
 const errorZip = document.querySelector("#errorZip");
 const nextButton = document.querySelector("#nextbutton");
 const payform = document.querySelector("#payment");
-
 
 function validateName(event) {
     event.preventDefault();
@@ -129,6 +127,121 @@ function checkAllFields() {
 nextButton.onclick = function showPayment(event) {
     event.preventDefault();
     payform.style.display = "flex";
+}
+}
+const vippsButton = document.querySelector("#vippsbutton");
+const creditButton = document.querySelector("#creditbutton");
+const vippsForm = document.querySelector (".vipps");
+const creditForm = document.querySelector (".credit");
+const paypalButton = document.querySelector("#paypalbutton");
+const lines = document.querySelector("#lines");
+
+vippsButton.onclick = function showVippsForm() {
+    vippsForm.style.display = "flex";
+    creditForm.style.display = "none";
+    paypalButton.style.display = "none";
+    lines.style.display = "none";
+
+
+const phone = document.querySelector("#phonePay");
+const errorPhone = document.querySelector("#errorPhonePay");
+const orderButton = document.querySelector("#orderVipps");
+const successMessage = document.querySelector("#successCard");
+const cartForm = document.querySelector("#shoppingCart");
+const payForm = document.querySelector("#payment");
+const addressForm = document.querySelector("#address");
+const welcomeSection = document.querySelector(".welcome");
+function validatePhone(event) {
+    event.preventDefault();
+    if (phone.value.length > 7) {
+        errorPhone.style.display = "";
+        orderButton.disabled = false;
+    }   else {
+        errorPhone.style.display = "inline";
+        orderButton.disabled = true;
+    }
+}
+phone.addEventListener("keyup", validatePhone);
+orderButton.onclick = function success(event) {
+    event.preventDefault();
+    cartForm.style.display = "none";
+    addressForm.style.display = "none";
+    payForm.style.display = "none";
+    welcomeSection.style.display = "none";
+    successMessage.style.display = "flex";
+}
+}
+creditButton.onclick = function showCreditForm(event) {
+    event.preventDefault();
+    creditForm.style.display = "flex";
+    vippsForm.style.display = "none";
+    paypalButton.style.display = "none";
+    lines.style.display = "none";
+
+
+const fullName = document.querySelector("#namePay");
+const cardNumber = document.querySelector("#cardNumber");
+const cvv = document.querySelector("#code");
+const errorName = document.querySelector("#errorNamePay");
+const errorNumber = document.querySelector("#errorNumber");
+const errorCvv = document.querySelector("#errorCvv");
+const orderButton = document.querySelector("#orderCredit");
+
+function validateName(event) {
+    event.preventDefault();
+    if (fullName.value.trim().length > 1) {
+        errorName.style.display = "";
+        checkAllFields();
+    }  else {
+        errorName.style.display = "inline";
+        orderButton.disabled = true;
+    } 
+}
+function validateNumber(event) {
+    event.preventDefault();
+    if (cardNumber.value.length === 16) {
+        errorNumber.style.display = "";
+        checkAllFields();
+    } else {
+        errorNumber.style.display = "inline";
+        orderButton.disabled = true;
+    }
+}
+function validateCvv(event) {
+    event.preventDefault();
+    if (cvv.value.length === 3) {
+        errorCvv.style.display = "";
+        checkAllFields();
+    }   else {
+        errorCvv.style.display = "inline";
+        orderButton.disabled = true;
+    }
+}
+fullName.addEventListener("input", validateName);
+cardNumber.addEventListener("input", validateNumber);
+cvv.addEventListener("input", validateCvv);
+
+function checkAllFields() {
+    const allFieldsValid = (
+        fullName.value.trim().length > 1 &&
+        cardNumber.value.length === 16 &&
+        cvv.value.length === 3
+    )
+    orderButton.disabled = !allFieldsValid;
+}
+const successMessage = document.querySelector("#successCard");
+const cartForm = document.querySelector("#shoppingCart");
+const payForm = document.querySelector("#payment");
+const addressForm = document.querySelector("#address");
+const welcomeSection = document.querySelector(".welcome");
+
+orderButton.onclick = function success(event) {
+    event.preventDefault();
+    cartForm.style.display = "none";
+    addressForm.style.display = "none";
+    payForm.style.display = "none";
+    welcomeSection.style.display = "none";
+    successMessage.style.display = "flex";
 }
 }
 
